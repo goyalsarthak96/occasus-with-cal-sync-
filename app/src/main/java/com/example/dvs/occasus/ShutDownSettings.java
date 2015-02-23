e0232974ff2519382daa34ffc33559322cb61174
goyalsarthak96 <goyalsarthak96@gmail.com>
2015-02-23 17:46:47 +0530

    Update ShutDownSettings.java

--- app/src/main/java/com/example/dvs/occasus/ShutDownSettings.java
+++ app/src/main/java/com/example/dvs/occasus/ShutDownSettings.java
@@ -7,7 +7,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-//comment to check push
+
 
 public class ShutDownSettings extends ActionBarActivity {
 
